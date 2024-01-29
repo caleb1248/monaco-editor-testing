8ade36675f0314bdf2b5f64c03438137645bf2de
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2024-01-29 13:01:45 +0000

    fix

--- src/monacoreact.tsx
+++ src/monacoreact.tsx
@@ -1,16 +1,16 @@
-import monaco from './setup';
-import * as vscode from 'vscode';
-import { createConfiguredEditor, createModelReference } from 'vscode/monaco';
-import '@codingame/monaco-vscode-theme-defaults-default-extension';
-import '@codingame/monaco-vscode-javascript-default-extension';
-import '@codingame/monaco-vscode-typescript-basics-default-extension';
+import monaco from "./setup";
+import * as vscode from "vscode";
+import { createConfiguredEditor, createModelReference } from "vscode/monaco";
+import "@codingame/monaco-vscode-theme-defaults-default-extension";
+import "@codingame/monaco-vscode-javascript-default-extension";
+import "@codingame/monaco-vscode-typescript-basics-default-extension";
 // import '@codingame/monaco-vscode-typescript-language-features-default-extension';
 
 // await initializeVscodeExtensions();
 
-const editorDiv = document.createElement('div');
-editorDiv.style.height = '100%';
-const uri = vscode.Uri.file('/path/to/index.ts');
+const editorDiv = document.createElement("div");
+editorDiv.style.height = "100%";
+const uri = vscode.Uri.file("/path/to/index.ts");
 
 monaco.editor.createModel('console.log("hi")', undefined, uri);
 const modelRef = await createModelReference(uri);
@@ -19,7 +19,6 @@ const editor = createConfiguredEditor(editorDiv, {
   model: modelRef.object.textEditorModel,
   automaticLayout: true,
 });
-console.log(await vscode.languages.getLanguages());
 
 export default function MonacoEditor() {
   return (
--- src/setup.ts
+++ src/setup.ts
@@ -1,54 +1,55 @@
-import * as monaco from 'monaco-editor';
-import './tools/crossOriginWorker';
-import getTextmateServiceOverride from '@codingame/monaco-vscode-textmate-service-override';
-import getThemeServiceOverride from '@codingame/monaco-vscode-theme-service-override';
-import getLanguagesServiceOverride from '@codingame/monaco-vscode-languages-service-override/languages';
-import { initialize as initializeMonacoServices } from 'vscode/services';
-import '@codingame/monaco-vscode-theme-defaults-default-extension';
-import '@codingame/monaco-vscode-javascript-default-extension';
-import '@codingame/monaco-vscode-typescript-basics-default-extension';
-import '@codingame/monaco-vscode-typescript-language-features-default-extension';
-import 'vscode/localExtensionHost';
-import { Worker } from './tools/crossOriginWorker';
+import * as monaco from "monaco-editor";
+import "./tools/crossOriginWorker";
+import getTextmateServiceOverride from "@codingame/monaco-vscode-textmate-service-override";
+import getThemeServiceOverride from "@codingame/monaco-vscode-theme-service-override";
+import getLanguagesServiceOverride from "@codingame/monaco-vscode-languages-service-override/languages";
+import { initialize as initializeMonacoServices } from "vscode/services";
+import getExtensionsServiceOverride from "@codingame/monaco-vscode-extensions-service-override";
+import "@codingame/monaco-vscode-theme-defaults-default-extension";
+import "@codingame/monaco-vscode-javascript-default-extension";
+import "@codingame/monaco-vscode-typescript-basics-default-extension";
+import "@codingame/monaco-vscode-typescript-language-features-default-extension";
+import "vscode/localExtensionHost";
+import { Worker } from "./tools/crossOriginWorker";
 
 type WorkerLoader = () => Worker;
 const workerLoaders: Partial<Record<string, WorkerLoader>> = {
   editorWorkerService: () =>
     new Worker(
-      new URL('monaco-editor/esm/vs/editor/editor.worker.js', import.meta.url),
-      { type: 'module' }
+      new URL("monaco-editor/esm/vs/editor/editor.worker.js", import.meta.url),
+      { type: "module" }
     ),
   textMateWorker: () =>
     new Worker(
       new URL(
-        '@codingame/monaco-vscode-textmate-service-override/worker',
+        "@codingame/monaco-vscode-textmate-service-override/worker",
         import.meta.url
       ),
-      { type: 'module' }
+      { type: "module" }
     ),
   outputLinkComputer: () =>
     new Worker(
       new URL(
-        '@codingame/monaco-vscode-output-service-override/worker',
+        "@codingame/monaco-vscode-output-service-override/worker",
         import.meta.url
       ),
-      { type: 'module' }
+      { type: "module" }
     ),
   languageDetectionWorkerService: () =>
     new Worker(
       new URL(
-        '@codingame/monaco-vscode-language-detection-worker-service-override/worker',
+        "@codingame/monaco-vscode-language-detection-worker-service-override/worker",
         import.meta.url
       ),
-      { type: 'module' }
+      { type: "module" }
     ),
   notebookEditorWorkerService: () =>
     new Worker(
       new URL(
-        '@codingame/monaco-vscode-notebook-service-override/worker',
+        "@codingame/monaco-vscode-notebook-service-override/worker",
         import.meta.url
       ),
-      { type: 'module' }
+      { type: "module" }
     ),
 };
 
@@ -66,7 +67,7 @@ await initializeMonacoServices({
   ...getLanguagesServiceOverride(),
   ...getTextmateServiceOverride(),
   ...getThemeServiceOverride(),
-  // ...getExtensionServiceOverride(),
+  ...getExtensionsServiceOverride(),
 });
 
 export default monaco;
